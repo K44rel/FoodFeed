bc333e663a746ad1a2c6df7f7e2159a0339adfff
kaarel <kaarel.loide@gmail.com>
2019-04-11 23:35:31 +0300

    Updates

---
+++ precache-manifest.afe2968c524bc970b82f2e79fb0ad021.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12f3add404290a82e57a",
+    "url": "/FoodFeed/static/js/runtime~main.17c9f1fc.js"
+  },
+  {
+    "revision": "faed03cd9ef522685748",
+    "url": "/FoodFeed/static/js/main.bcd419bf.chunk.js"
+  },
+  {
+    "revision": "3a1de918289cdb693dc3",
+    "url": "/FoodFeed/static/js/2.3821f224.chunk.js"
+  },
+  {
+    "revision": "faed03cd9ef522685748",
+    "url": "/FoodFeed/static/css/main.a13ba511.chunk.css"
+  },
+  {
+    "revision": "78cf01b09d2dc256c67819fc1213ce3c",
+    "url": "/FoodFeed/index.html"
+  }
+];
